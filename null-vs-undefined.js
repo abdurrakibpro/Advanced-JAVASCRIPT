d6e28d7aa261b2c5bfc6649c566545627b32f439
unknown <abdurrakib.bs@gmail.com>
2020-03-05 19:06:56 +0600

    commit 2

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"smart dude", phone: 23534564};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+null ages = [1, 5, 6]
+console.log(ages[11]);
