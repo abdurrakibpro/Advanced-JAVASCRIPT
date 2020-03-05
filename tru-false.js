6b8367c20a7cf5357d53638643b101698ff0fec8
unknown <abdurrakib.bs@gmail.com>
2020-03-05 18:51:30 +0600

    first commit

---
+++ tru-false.js
@@ -0,0 +1,24 @@
+// Truthy
+// "0", " ", []
+// -------------
+// Falsy
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+
+
+
+
+
+let name = [];
+console.log(name);
+if(name){
+    HTMLFormControlsCollection.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
